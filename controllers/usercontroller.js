a95cd876272ed83379d5fd5eaab99a127e0b1c61
gusainDeekshu <gusaindeekshant@gmail.com>
2025-01-27 04:07:27 +0530

    Authorization Added For Fetching User Data

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -8,6 +8,34 @@ const albummodel = require("../model/albummodel");
 //add article
 require("dotenv").config();
 // login user
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).json({
+          status: 401,
+          data: null,
+          message: "Unauthorized Access",
+          error: null,
+        });
+      } else {
+        req.body.id = decode.id;
+        console.log( decode.id)
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: 400,
+      data: null,
+      message: "Bad Request",
+      error: null,
+    });
+  }
+};
 
 const loginuser = async (req, res) => {
   const { email, password } = req.body;
